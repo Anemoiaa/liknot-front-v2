e6382b90ac308ff6852e1d97cb1b7d055e86ee0d
Anemoiaa <georgiypetroff@gmail.com>
2023-05-19 15:27:57 +0400

    feat: open menu on click on the menu item

--- assets/js/dashboard-menu.js
+++ assets/js/dashboard-menu.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // open menu
     $('.burger-menu').on('click', function() {
         let isOpened = $(this).attr('data-opened') === 'false';
         $(this).attr('data-opened', isOpened);
@@ -10,6 +11,14 @@ $(document).ready(function() {
     });
     $('.menu .close').on('click', function() {
         closeMenu();
+    });
+
+
+    // open menu on click on menu item
+    $('.menu .menu__list .menu__item').on('click', function() {
+        let isOpened = $('.burger-menu').attr('data-opened') === 'false';
+        $(this).attr('data-opened', isOpened);
+        if(isOpened) openMenu();
     });
 
     function openMenu () {
